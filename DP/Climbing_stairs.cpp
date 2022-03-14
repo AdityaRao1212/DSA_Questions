11feb618fe36d5f1a4f4f02e51923406faaa3105
Aditya Rao <adityarao122k@gmail.com>
2022-03-15 00:33:41 +0530

    Code for Climbing Stairs.

---
+++ DP/Climbing_stairs.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    int climbStairs(int n)
+    {
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1, dp[1] = 2;
+        for (int i = 2; i < n; i++)
+        {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n - 1];
+    }
+};
