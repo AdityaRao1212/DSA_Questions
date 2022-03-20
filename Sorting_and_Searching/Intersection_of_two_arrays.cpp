84d112e1644f5a801d378d1edeb7fba041eecbe4
Aditya Rao <adityarao122k@gmail.com>
2022-03-20 19:14:31 +0530

    Intersection Of two arrays II.

---
+++ Sorting_and_Searching/Intersection_of_two_arrays.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> intersect(vector<int> &nums1, vector<int> &nums2)
+    {
+        // First sort both the arrays.
+        // Keep two pointers and solve the question.
+        vector<int> res;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        int n = nums1.size(), m = nums2.size(), i = 0, j = 0;
+        while (i < n && j < m)
+        {
+            if (nums1[i] == nums2[j])
+            { // If the numbers are same add to the vector and increment the pointers by one.
+                res.push_back(nums1[i]);
+                i++, j++;
+            }
+            else if (nums1[i] > nums2[j])
+            { // If first one is greater increment the other pointer.
+                j++;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return res; // resultant vector.
+    }
+};
